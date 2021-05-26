fa409025c6d42ffc0fe7e5eaa491b8742926f5dc
hamedghd <h.gharibdoost@gmail.com>
2021-05-27 00:12:33 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -13,18 +13,18 @@ function newItem () {
   }
   // 2. Crossing out an item from the list of items:
   function crossOut () {
-   li.toggleClass('strike');
+    li.toggleClass('strike');
   }
   li.on('dblclick', crossOut);
   // 3(i). Adding the delete button "X":
   let crossOutButton = $('<crossOutButton>X</crossOutButton>');
- 	li.append(crossOutButton);
+  li.append(crossOutButton);
 
- 	crossOutButton.on('click', deleteListItem);
+  crossOutButton.on('click', deleteListItem);
   // 3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
-    function deleteListItem(){
-  		li.addClass('delete');
-  	}
+  function deleteListItem () {
+    li.addClass('delete');
+  }
   // 4. Reordering the items:
   $('#list').sortable();
 }
